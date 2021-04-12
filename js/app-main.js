6d064329190a061f920892fa98dee181b313e13d
motsenko <motsenkolive@gmail.com>
2021-04-12 15:53:14 +0300

    5

--- js/app-main.js
+++ js/app-main.js
@@ -2459,40 +2459,43 @@ if (animItems.length > 0) {
 
 //lazy-load
 "use strict"
+function lazyImage() {
+	const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
+	const windowHeight = document.documentElement.clientHeight;
 
-const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
-const windowHeight = document.documentElement.clientHeight;
+	let lazyImagesPositions = [];
+	if (lazyImages.length > 0) {
+		lazyImages.forEach(img => {
+			if (img.dataset.src || img.dataset.srcset) {
+				lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+				lazyScrollCheck();
+			}
+		});
+	}
 
-let lazyImagesPositions = [];
-if (lazyImages.length > 0) {
-	lazyImages.forEach(img => {
-		if (img.dataset.src || img.dataset.srcset) {
-			lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+	window.addEventListener("scroll", lazyScroll);
+
+	function lazyScroll() {
+		if (document.querySelectorAll('img[data-src],source[data-srcset]').length > 0) {
 			lazyScrollCheck();
 		}
-	});
-}
-
-window.addEventListener("scroll", lazyScroll);
-
-function lazyScroll() {
-	if (document.querySelectorAll('img[data-src],source[data-srcset]').length > 0) {
-		lazyScrollCheck();
 	}
-}
 
-function lazyScrollCheck() {
-	let imgIndex = lazyImagesPositions.findIndex(
-		item => pageYOffset > item - windowHeight
-	);
-	if (imgIndex >= 0) {
-		if (lazyImages[imgIndex].dataset.src) {
-			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-			lazyImages[imgIndex].removeAttribute('data-src');
-		} else if (lazyImages[imgIndex].dataset.srcset) {
-			lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
-			lazyImages[imgIndex].removeAttribute('data-srcset');
+	function lazyScrollCheck() {
+		let imgIndex = lazyImagesPositions.findIndex(
+			item => pageYOffset > item - windowHeight
+		);
+		if (imgIndex >= 0) {
+			if (lazyImages[imgIndex].dataset.src) {
+				lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+				lazyImages[imgIndex].removeAttribute('data-src');
+			} else if (lazyImages[imgIndex].dataset.srcset) {
+				lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+				lazyImages[imgIndex].removeAttribute('data-srcset');
+			}
+			delete lazyImagesPositions[imgIndex];
 		}
-		delete lazyImagesPositions[imgIndex];
 	}
 }
+
+document.addEventListener('DOMContentLoaded', lazyImage);
